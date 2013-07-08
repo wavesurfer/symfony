cb289acc18b77a99e2f1f660b6b7a2d0533e5827
wavesurfer <hans.lieberich@freenet.de>
2013-07-08 16:45:46 +0200

    further day 11 changes

--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -93,3 +93,30 @@ $browser->info('3 - Post a Job page')->
     isParameter('action', 'create')->
   end()
 ;
+
+$browser->info('  3.6 - A job validity cannot be extended before the job expires soon')->
+  createJob(array('position' => 'FOO4'), true)->
+  call(sprintf('/job/%s/extend', $browser->getJobByPosition('FOO4')->getToken()), 'put', array('_with_csrf' => true))->
+  with('response')->begin()->
+    isStatusCode(404)->
+  end()
+;
+ 
+$browser->info('  3.7 - A job validity can be extended when the job expires soon')->
+  createJob(array('position' => 'FOO5'), true)
+;
+ 
+$job = $browser->getJobByPosition('FOO5');
+$job->setExpiresAt(date('Y-m-d'));
+$job->save();
+ 
+$browser->
+  call(sprintf('/job/%s/extend', $job->getToken()), 'put', array('_with_csrf' => true))->
+  with('response')->isRedirected()
+;
+ 
+$job->refresh();
+$browser->test()->is(
+  $job->getDateTimeObject('expires_at')->format('y/m/d'),
+  date('y/m/d', time() + 86400 * sfConfig::get('app_active_days'))
+);
