85c3247d555da43b6b5c3bee812fd6924e9812f7
wavesurfer <hans.lieberich@freenet.de>
2013-07-09 15:42:29 +0200

    backend user management

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -18,16 +18,17 @@
         </h1>
       </div>
  
+<!--    // apps/backend/templates/layout.php-->
+    <?php if ($sf_user->isAuthenticated()): ?>
       <div id="menu">
         <ul>
-          <li>
-            <?php echo link_to('Jobs', 'jobeet_job') ?>
-          </li>
-          <li>
-            <?php echo link_to('Categories', 'jobeet_category') ?>
-          </li>
+          <li><?php echo link_to('Jobs', 'jobeet_job') ?></li>
+          <li><?php echo link_to('Categories', 'jobeet_category') ?></li>
+          <li><?php echo link_to('Users', 'sf_guard_user') ?></li>
+          <li><?php echo link_to('Logout', 'sf_guard_signout') ?></li>
         </ul>
       </div>
+    <?php endif ?>
  
       <div id="content">
         <?php echo $sf_content ?>
